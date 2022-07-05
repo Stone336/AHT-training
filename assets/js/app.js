4e06bfb93f068e216b3ca15140cfb8ec760b9a28
Stone336 <hoangquocnam2001@gmail.com>
2022-07-05 14:30:43 +0700

    fontend

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    searchToggle = document.querySelector(".searchToggle"),
+    sliderbarOpen = document.querySelector(".sliderbarOpen"),
+    sliderbarClose = document.querySelector(".sliderbarClose");
+
+    // SEARCH
+searchToggle.addEventListener("click" , () =>{
+    searchToggle.classList.toggle("active");
+});
+    // OPEN SLIDER
+sliderbarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sliderbarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
+$(document).ready(function(){
+    $('.autoplay').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+    });
+    $('.toggle') .click(function(){
+        $('.main-menu').slideToggle('');
+    });
+});
